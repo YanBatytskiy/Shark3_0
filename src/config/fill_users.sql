dfff238a6fbff96a55291dfd04f2deadb3bdfd21
YanBatytskiy <YanBatytskiy@gmail.com>
2025-08-09 00:37:25 +0300

    sql

---
+++ src/config/fill_users.sql
@@ -0,0 +1,8 @@
+insert into users (login, name, email, phone) values ('a', 'Sasha', '...@gmail.com','111');
+insert into users (login, name, email, phone) values ('e', 'Elena', '...@gmail.com','111');
+insert into users (login, name, email, phone) values ('s', 'Sergei', '...@gmail.com','111');
+insert into users (login, name, email, phone) values ('v', 'Vitaliy', '...@gmail.com','111');
+insert into users (login, name, email, phone) values ('m', 'Mariya', '...@gmail.com','111');
+insert into users (login, name, email, phone) values ('f', 'Fedor', '...@gmail.com','111');
+insert into users (login, name, email, phone) values ('ver', 'Vera', '...@gmail.com','111');
+insert into users (login, name, email, phone) values ('y', 'Yakov', '...@gmail.com','111');
